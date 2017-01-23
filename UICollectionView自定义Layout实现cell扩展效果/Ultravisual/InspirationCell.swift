15e950b3659a51e5aba367c49ed5e8a19b101471
lisen <15105164911@163.com>
2017-01-23 17:12:30 +0800

    自定义UICollectionViewLayout实现cell扩展效果

---
+++ UICollectionView自定义Layout实现cell扩展效果/Ultravisual/InspirationCell.swift
@@ -0,0 +1,41 @@
+//
+//  TutorialCell.swift
+//  RWDevCon
+//
+//  Created by Mic Pringle on 27/02/2015.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class InspirationCell: UICollectionViewCell {
+  
+  @IBOutlet fileprivate weak var imageView: UIImageView!
+  @IBOutlet fileprivate weak var imageCoverView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+  
+  var inspiration: Inspiration? {
+    didSet {
+      if let inspiration = inspiration {
+        imageView.image = inspiration.backgroundImage
+        titleLabel.text = inspiration.title
+      }
+    }
+  }
+  
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.apply(layoutAttributes)
+        
+        let standardHeight = UltravisualLayoutConstants.Cell.standardHeight
+        let featuredHeight = UltravisualLayoutConstants.Cell.featuredHeight
+        
+        let delta = 1 - ((featuredHeight - frame.height) / (featuredHeight - standardHeight))
+        
+        let minAlpha: CGFloat = 0.3
+        let maxAlpha: CGFloat = 0.75
+        imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+        
+        let scale = max(delta, 0.5)
+        titleLabel.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+}
